ac33aedbc537662683c6935eb56ca05f887cc09d
LeandroAgristola <leandro.agristola@gmail.com>
2024-11-15 18:09:01 -0300

    Terminando estilos - listo para probar funcionalidades

--- projet_03/mail/mail/forms.py
+++ projet_03/mail/mail/forms.py
@@ -13,26 +13,25 @@ class UserRegistrationForm(UserCreationForm):
         fields = ["first_name", "last_name", "email", "profile_picture", "password1", "password2"]
 
     def __init__(self, *args, **kwargs):
-        # Check if the instance is being edited by seeing if `instance` is provided
         editing = kwargs.get('instance') is not None
         super().__init__(*args, **kwargs)
-        
+
         if editing:
-            # If editing, remove the email field to prevent changes
             self.fields.pop("email")
+
+        # Añadir la clase 'form-control' a cada campo
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
 
     def clean_profile_picture(self):
         profile_picture = self.cleaned_data.get("profile_picture")
 
-        # Verificar si el campo es opcional y está vacío
         if not profile_picture:
             return profile_picture
-        
-        # Validar tipo de archivo de imagen
+
         if profile_picture.content_type not in ["image/jpeg", "image/png"]:
             raise ValidationError("Solo se permiten imágenes JPEG y PNG.")
 
-        # Validar tamaño máximo de la imagen (2 MB en este ejemplo)
         max_size_kb = 2048  # 2 MB en KB
         if profile_picture.size > max_size_kb * 1024:
             raise ValidationError(f"El tamaño máximo de la imagen es de {max_size_kb / 1024} MB.")
@@ -41,7 +40,6 @@ class UserRegistrationForm(UserCreationForm):
 
     def save(self, commit=True):
         user = super().save(commit=False)
-        # Solo asigna email como username en el caso de registro
         if not self.instance.pk:  # Si el usuario aún no existe en la base de datos
             user.username = self.cleaned_data["email"]
         if commit:
--- projet_03/mail/mail/urls.py
+++ projet_03/mail/mail/urls.py
@@ -13,4 +13,6 @@ urlpatterns = [
     path("emails", views.compose, name="compose"),
     path("emails/<int:email_id>", views.email, name="email"),
     path("emails/<str:mailbox>", views.mailbox, name="mailbox"),
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
