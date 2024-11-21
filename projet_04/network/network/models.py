ce36cc6972fa590485d7556abfce57ee3701462b
LeandroAgristola <leandro.agristola@gmail.com>
2024-11-21 19:28:22 -0300

    configuracando modelos de registro y agregando logos

--- projet_04/network/network/models.py
+++ projet_04/network/network/models.py
@@ -1,6 +1,10 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-
 class User(AbstractUser):
-    pass
+    # Stores the user's first name with a maximum length of 30 characters
+    first_name = models.CharField(max_length=30)
+    # Stores the user's last name with a maximum length of 30 characters
+    last_name = models.CharField(max_length=30)
+    # Optional profile picture for the user, stored in 'profile_pics/' directory
+    profile_picture = models.ImageField(upload_to='profile_pics/', null=True, blank=True)
