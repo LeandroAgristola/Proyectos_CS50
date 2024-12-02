faabddbbe9ecccc169558e827e889437be46a68a
LeandroAgristola <leandro.agristola@gmail.com>
2024-12-02 17:45:57 -0300

    Modificacmos codificacion de wiki

--- projet_01/wiki/encyclopedia/util.py
+++ projet_01/wiki/encyclopedia/util.py
@@ -15,23 +15,25 @@ def list_entries():
 
 def save_entry(title, content):
     """
-    Saves an encyclopedia entry, given its title and Markdown
-    content. If an existing entry with the same title already exists,
-    it is replaced.
+    Saves an encyclopedia entry, given its title and Markdown content.
+    If an existing entry with the same title already exists, it is replaced.
     """
     filename = f"entries/{title}.md"
     if default_storage.exists(filename):
         default_storage.delete(filename)
-    default_storage.save(filename, ContentFile(content))
+    content_file = ContentFile(content.encode('utf-8'))  
+    default_storage.save(filename, content_file)
 
 
 def get_entry(title):
     """
-    Retrieves an encyclopedia entry by its title. If no such
-    entry exists, the function returns None.
+    Retrieves an encyclopedia entry by its title. If no such entry exists,
+    the function returns None.
     """
     try:
         f = default_storage.open(f"entries/{title}.md")
-        return f.read().decode("utf-8")
+        return f.read().decode('utf-8')
     except FileNotFoundError:
         return None
+    except UnicodeDecodeError:
+        return "Error: Unable to decode file."
