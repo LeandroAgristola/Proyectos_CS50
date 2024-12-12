49191855fc695e181147f6bc79770641d741f184
LeandroAgristola <leandro.agristola@gmail.com>
2024-12-11 23:02:06 -0300

    final project!

--- final_project/capstone/realestate/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import development
-
-# Register your models here.
-
-class developmentAdmin(admin.ModelAdmin):
-    readonly_fields=('created','updated') 
-
-admin.site.register(development, developmentAdmin)
--- final_project/capstone/realestate/static/js/startmaps.js
+++
@@ -1,6 +0,0 @@
-function iniciarMap() {
-    const map = new google.maps.Map(document.getElementById('map'), {
-        center: { lat: -34.6037, lng: -58.3816 },
-        zoom: 12
-    });
-}
